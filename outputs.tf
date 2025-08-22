7e07e5c6afde483ac9b6c9f2f1aec67f1b8a04e3
mohnish-khanna <mohnish.k@webkorps.com>
2025-08-22 13:35:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -75,3 +75,34 @@ output "eks_node_group_arn" {
   description = "EKS node group ARN"
   value       = module.eks.node_group_arn
 }
+
+output "eks_cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+  sensitive   = true
+}
+
+output "eks_cluster_version" {
+  description = "EKS cluster version"
+  value       = module.eks.cluster_version
+}
+
+output "eks_node_group_arn" {
+  description = "EKS node group ARN"
+  value       = module.eks.node_group_arn
+}
+
+output "kubectl_config" {
+  description = "kubectl config command"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
